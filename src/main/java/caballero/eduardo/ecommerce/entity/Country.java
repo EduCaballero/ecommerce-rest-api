26988ec5052bc2f915f84b86d2ae9c05bba8e5e7
Edu Caballero <edustrife@gmail>
2022-07-09 22:06:05 +0200

    Country and State models and repositories

---
+++ src/main/java/caballero/eduardo/ecommerce/entity/Country.java
@@ -0,0 +1,24 @@
+package caballero.eduardo.ecommerce.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String code;
+    private String name;
+    @OneToMany(mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
+
+
+
+}
